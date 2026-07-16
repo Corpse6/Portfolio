7c2012e48d855578b3a9f54b00f5cd383d262f9a
Corpse6 <navinmartin555@gmail.com>
2026-07-16 22:25:01 +0530

    Refactor particle generation in script.js
    Refactor particle creation loop and add animation delay.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-console.log("Corpse Starlight awakened");
-
-
-for(let i = 0; i < 50; i++){
+for(let i = 0; i < 150; i++){
 
     let particle = document.createElement("div");
 
     particle.className = "particle";
 
-    particle.style.left = Math.random() * 100 + "vw";
-    particle.style.top = Math.random() * 100 + "vh";
+    particle.style.left =
+    Math.random() * 100 + "vw";
+
+    particle.style.top =
+    Math.random() * 100 + "vh";
+
+    particle.style.animationDelay =
+    Math.random() * 6 + "s";
 
     document.body.appendChild(particle);
 
