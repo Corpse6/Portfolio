13f98c71b1788cb76916dce5656e7ef04aa3a5ad
Corpse6 <navinmartin555@gmail.com>
2026-07-16 22:54:32 +0530

    Refactor particle creation and animation properties

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
+console.log("☠ Corpse Starlight Grimoire Awakened ☠");
+
+
+
+function awaken(){
+
+alert(
+"☠ GRIMOIRE AWAKENED ☠\n\n" +
+"Corpse Starlight begins his journey.\n" +
+"New summons will be created."
+);
+
+}
+
+
+
+
+
+// SOUL PARTICLES
+
+
 for(let i = 0; i < 150; i++){
 
-    let particle = document.createElement("div");
 
-    particle.className = "particle";
+    let particle=document.createElement("div");
+
+
+    particle.className="particle";
+
 
     particle.style.left =
-    Math.random() * 100 + "vw";
+    Math.random()*100+"vw";
+
 
     particle.style.top =
-    Math.random() * 100 + "vh";
+    Math.random()*100+"vh";
+
+
 
     particle.style.animationDelay =
-    Math.random() * 6 + "s";
+    Math.random()*6+"s";
+
+
+
+    particle.style.animationDuration =
+    (4+Math.random()*6)+"s";
+
+
 
     document.body.appendChild(particle);
+
 
 }
