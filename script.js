ab8f526fc26cee42f86e7bd2063dd2a44c130c5d
Corpse6 <navinmartin555@gmail.com>
2026-07-16 22:11:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,11 @@ let particle=document.createElement("div");
 
 particle.className="particle";
 
-particle.style.left=
-Math.random()*100+"%";
+particle.style.left =
+Math.random()*100 + "vw";
+
+particle.style.top =
+Math.random()*100 + "vh";
 
 
 particle.style.animationDelay=
