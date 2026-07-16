6c06617d1388800893509ff8a6e50520eaf42e12
Corpse6 <navinmartin555@gmail.com>
2026-07-16 21:59:46 +0530

    Implement awaken function and particle generation
    Added a function to awaken and create particles on the screen.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const menuToggle = document.getElementById('menuToggle');
-const navLinks = document.getElementById('navlinks');
+function awaken(){
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
+alert(
+"☠ GRIMOIRE AWAKENED ☠\n\nCorpse Starlight has begun his journey."
+);
+
+}
+
+
+
+for(let i=0;i<120;i++){
+
+let particle=document.createElement("div");
+
+particle.className="particle";
+
+particle.style.left=
+Math.random()*100+"%";
+
+
+particle.style.animationDelay=
+Math.random()*5+"s";
+
+
+document.body.appendChild(particle);
+
+}
