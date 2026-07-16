cc1d9304c2d09b90571081a15b25b086e5e7de16
Corpse6 <navinmartin555@gmail.com>
2026-07-16 22:21:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-console.log("Necromancer script awakened");
-function awaken(){
-
-alert(
-"☠ GRIMOIRE AWAKENED ☠\n\nCorpse Starlight has begun his journey."
-);
-
-}
+console.log("Corpse Starlight awakened");
 
 
-for(let i = 0; i < 150; i++){
+for(let i = 0; i < 50; i++){
 
     let particle = document.createElement("div");
 
     particle.className = "particle";
 
-
-    particle.style.left =
-    Math.random() * window.innerWidth + "px";
-
-
-    particle.style.top =
-    Math.random() * window.innerHeight + "px";
-
-
-    particle.style.animationDelay =
-    Math.random() * 8 + "s";
-
+    particle.style.left = Math.random() * 100 + "vw";
+    particle.style.top = Math.random() * 100 + "vh";
 
     document.body.appendChild(particle);
 
